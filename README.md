c2f53ac47cf80c2689ec23ff23a9cdc77cfbaeea
Ellery Li <elleryli@Ellerys-Mini.lan>
2017-12-31 15:46:18 +0800

    Finish project 1.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ud036_StarterCode
 Source code for a Movie Trailer website.
+
+
+# Project I : Movie Trailer Website
+
+## Project Overview
+
+This project is to build a server-side website to store a list of the favorite movies, including box art imgagery and a movie trailer URL. It will use the code to generate a static web page to allow visitors to browse movies and watch the trailers.
+
+## How to run this program:
+
+
+### Install python
+
+You need to install python 2.7 or above at first:
+
+* For Windows: go to <https://www.python.org/downloads/> to download and install it.
+
+* For CentOS:  ``` yum install python```
+
+* For Mac OS: ``` brew install python```
+
+* For other Linux operating systems:  ``` apt-get install python```
+
+
+### Run the program
+Type following command to your terminal to run this program:
+```
+ python entertainment_center.py
+```
+
+
+
+### Resources
+- Google Google Python Style Guide: <https://google.github.io/styleguide/pyguide.html>
+- The Python Language Reference: <https://docs.python.org/2/reference/index.html>
---
+++ entertainment_center.py
@@ -0,0 +1,46 @@
+#entertainment_center.py is using to create movie instances though fresh_tomatoes.py
+
+import media
+import fresh_tomatoes
+
+#movie list:
+# - Despicable Me2
+# - Coco
+# - Forrest Gump
+# - Dead Poets Society
+# - WALL-E
+# - Inside Out
+
+
+despicable_me2 = media.Movie("Despicable Me2",
+						"",
+						"https://upload.wikimedia.org/wikipedia/en/2/29/Despicable_Me_2_poster.jpg",
+						"https://www.youtube.com/watch?v=yM9sKpQOuEw" )
+
+coco = media.Movie("Coco",
+						"",
+						"https://upload.wikimedia.org/wikipedia/en/9/98/Coco_%282017_film%29_poster.jpg",
+						"https://www.youtube.com/watch?v=xlnPHQ3TLX8" )
+
+forrest_gump = media.Movie("Forrest Gump",
+						"",
+						"https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
+						"https://www.youtube.com/watch?v=bLvqoHBptjg" )
+
+dead_poets_society = media.Movie("Dead Poets Society",
+						"",
+						"https://upload.wikimedia.org/wikipedia/zh/4/49/Dead_poets_society.jpg",
+						"https://www.youtube.com/watch?v=4lj185DaZ_o" )
+
+wall_e = media.Movie("WALL-E",
+						"",
+						"https://upload.wikimedia.org/wikipedia/en/c/c2/WALL-Eposter.jpg",
+						"https://www.youtube.com/watch?v=8-_9n5DtKOc" )
+
+inside_out = media.Movie("Inside Out",
+						"",
+						"https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg",
+						"https://www.youtube.com/watch?v=seMwpP0yeu4" )
+
+movies = [despicable_me2, coco, forrest_gump, dead_poets_society, wall_e, inside_out]
+fresh_tomatoes.open_movies_page(movies)
